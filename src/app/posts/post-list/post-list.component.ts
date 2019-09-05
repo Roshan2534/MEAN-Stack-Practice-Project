95b61cc33ceaec85260b99ab2e16ac9c63e9e4b7
Your Name <you@example.com>
2019-09-05 14:17:28 +0530

    Improved UI

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts=[
   //   {title: 'First Post', content:'This is the First Post\'s Content '},
   //   {title: 'Second Post', content:'This is the Second Post\'s Content '},
   //   {title: 'Third Post', content:'This is the Third Post\'s Content '}
   // ];
-  @Input() posts: Post[] = [];
+   posts: Post[] = [];
+   private postsSub: Subscription;
 
 
-  constructor() { }
+  constructor(public postService: PostsService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getpostUpdate().subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 
 }
