f8f4bbec99988cc0873a907c2de137443fb5df20
Your Name <you@example.com>
2019-09-10 18:00:16 +0530

    Added Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const mongoose = require('mongoose');
 
+const Post = require('./models/post');
 const app = express();
+
+mongoose.connect("mongodb+srv://Roshan:ciLLMlzZNy6vCVuB@cluster0-amid5.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(()=>{
+  console.log('Connected to database');
+})
+.catch(()=>{
+  console.log('Connection failed!')
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,8 +25,11 @@ app.use((req,res,next)=>{
 });
 
 app.post("/api/posts",(req,res,next)=>{
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message:'Post added successfully!'
   })
